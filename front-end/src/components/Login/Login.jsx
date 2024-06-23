3182b8f6d6c53e16960bef9bd61b5cc40a976ffd
amaljithmk07 <amaljithmk123@gmail.com>
2024-06-23 21:14:53 +0530

    Loader set for login page

--- front-end/src/components/Login/Login.jsx
+++ front-end/src/components/Login/Login.jsx
@@ -3,9 +3,13 @@ import "./Login.css";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import BASE_URI from "../Constant/Constant";
+import Loader from "../Loader/Loader";
 const Login = () => {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({});
+
+  ////Loader
+  const [loader, setLoader] = useState(false);
 
   //////////
   const loginFormDataHandler = (e) => {
@@ -18,46 +22,53 @@ const Login = () => {
   ///////////
   const formSubmit = (e) => {
     e.preventDefault();
+    setLoader(true);
     axios
       .post(`${BASE_URI}/api/login/`, formData)
       .then((data) => {
         console.log(data);
         sessionStorage.setItem("token", data.data.token);
         navigate("/home");
+        setLoader(false);
       })
       .catch((err) => {
         console.log(err);
+        setLoader(false);
       });
   };
   {
   }
   return (
     <div>
-      <div className="login-main-body">
-        <form action="" className="login-form-body">
-          <div className="login-form-title">LOGIN</div>
-          <input
-            type="text"
-            name="email"
-            placeholder="Email"
-            onChange={loginFormDataHandler}
-            className="login-form-input"
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            onChange={loginFormDataHandler}
-            className="login-form-input"
-          />
-          <button onClick={formSubmit} className="login-form-btn">
-            Submit
-          </button>
-          <div>
-            New to Bloogie? <Link to={"/register"}>Sign up</Link>
-          </div>
-        </form>{" "}
-      </div>
+      {loader == true ? (
+        <Loader />
+      ) : (
+        <div className="login-main-body">
+          <form action="" className="login-form-body">
+            <div className="login-form-title">LOGIN</div>
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              onChange={loginFormDataHandler}
+              className="login-form-input"
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={loginFormDataHandler}
+              className="login-form-input"
+            />
+            <button onClick={formSubmit} className="login-form-btn">
+              Submit
+            </button>
+            <div>
+              New to Bloogie? <Link to={"/register"}>Sign up</Link>
+            </div>
+          </form>{" "}
+        </div>
+      )}
     </div>
   );
 };
--- front-end/src/components/Register/Register.jsx
+++ front-end/src/components/Register/Register.jsx
@@ -3,9 +3,13 @@ import "./Register.css";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import BASE_URI from "../Constant/Constant";
+import Loader from "../Loader/Loader";
 
 const Register = () => {
   const navigate = useNavigate();
+  ////Loader
+  const [loader, setLoader] = useState(false);
+
   const [registerFormData, setRegisterFormData] = useState({});
   /////
   const registerformDataHandler = (e) => {
@@ -16,57 +20,65 @@ const Register = () => {
   ///////////
   const registerFormSubmit = (e) => {
     e.preventDefault();
+    setLoader(true);
+
     axios
       .post(`${BASE_URI}/api/register/`, registerFormData)
       .then((data) => {
         console.log(data);
         navigate("/");
+        setLoader(false);
       })
       .catch((err) => {
         console.log(err);
+        setLoader(false);
       });
   };
   return (
     <div>
-      <div className="register-main-body">
-        <form action="" className="register-form-body">
-          <div className="register-form-title">Register</div>
-          <input
-            type="text"
-            name="name"
-            placeholder="Name"
-            onChange={registerformDataHandler}
-            className="register-form-input"
-          />
-          <input
-            type="text"
-            name="email"
-            placeholder="Email"
-            onChange={registerformDataHandler}
-            className="register-form-input"
-          />
-          <input
-            type="passwoord"
-            name="password"
-            placeholder="Password"
-            className="register-form-input"
-            onChange={registerformDataHandler}
-          />
-          <input
-            type="text"
-            name="phone"
-            placeholder="Phone"
-            onChange={registerformDataHandler}
-            className="register-form-input"
-          />
-          <button onClick={registerFormSubmit} className="register-form-btn">
-            Submit
-          </button>
-          <div>
-            Already registered on Bloogie? <Link to={"/"}>Sign in</Link>
-          </div>
-        </form>
-      </div>
+      {loader == true ? (
+        <Loader />
+      ) : (
+        <div className="register-main-body">
+          <form action="" className="register-form-body">
+            <div className="register-form-title">Register</div>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              onChange={registerformDataHandler}
+              className="register-form-input"
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              onChange={registerformDataHandler}
+              className="register-form-input"
+            />
+            <input
+              type="passwoord"
+              name="password"
+              placeholder="Password"
+              className="register-form-input"
+              onChange={registerformDataHandler}
+            />
+            <input
+              type="text"
+              name="phone"
+              placeholder="Phone"
+              onChange={registerformDataHandler}
+              className="register-form-input"
+            />
+            <button onClick={registerFormSubmit} className="register-form-btn">
+              Submit
+            </button>
+            <div>
+              Already registered on Bloogie? <Link to={"/"}>Sign in</Link>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
